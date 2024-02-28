d3525b02d622bdd2a936f2d0d7af8b571fa289bc
Draven Grondona <160979453+dgrondona@users.noreply.github.com>
2024-02-27 18:32:55 -0800

    Create gameconfig.py
    Added game configuration files from project

---
+++ gameconfig.py
@@ -0,0 +1,63 @@
+# keybinds
+
+flapKey = 'space'
+
+abilityKey = 'm'
+
+#gravity
+
+gravityConst = 1 # gravity constant
+
+gravityIncrease = 0.06 # gravity acceleration
+
+# game values
+
+speed = 1 # speed of game
+
+difIncrease = 0.1 # how the game speeds up
+
+flapHeight = 30 # how high the turtle will flap
+
+# points
+
+pearlPoints = 1 # points received from pearls
+
+polePoints = 1 # points received from making it through poles
+
+# abilities
+
+cooldownLength = 3 # amount of points needed until cooldown is over
+
+decayRate = 1 # chance that auto pilot will turn of (out of 1000)
+
+# other
+
+currentHandicap = 2 # how much harder it is to flap in current zones
+
+# cost of skins
+
+costOfRock = 10
+
+costOfSub = 20
+
+costOfTorpedo = 35
+
+costOfPizza = 55
+
+costOfMoonlander = 75
+
+costOfTimeMachine = 100
+
+costOfUrchin = 125
+
+costOfOtter = 150
+
+# skins owned
+
+unlockedSkins = ["turtle"]
+
+# fonts
+
+gameFont = "font name"
+
+fontSize = 14
